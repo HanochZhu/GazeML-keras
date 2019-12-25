d4b3a2365998a93d6a67338fee7e67f3feed4f2a
Hongqiang.zhu <943264652@qq.com>
2019-12-25 16:34:06 +0800

    upgrade example scripts

--- example.py
+++ example.py
@@ -29,28 +29,48 @@ right_eye_xy = np.array([lms[5], lms[0]])
 
 dist_eyes = np.linalg.norm(left_eye_xy - right_eye_xy)
 # bounding box
-# eye_bbox_w = (dist_eyes / 1.25)
-# eye_bbox_h = (eye_bbox_w * 0.6)
+eye_bbox_w = (dist_eyes / 1.25)
+eye_bbox_h = (eye_bbox_w * 0.6)
 
 draw = input_img.copy()
 
-for i, lm in enumerate([left_eye_xy, right_eye_xy]):
-    draw = cv2.circle(draw, (int(lm[1]), int(lm[0])), 10, (255*i,255*(1-i),0), -1)
-    print(int(lm[1]), int(lm[0])) # x,y
-    
+left_bboxx = left_eye_xy[0]-eye_bbox_h//2
+left_bboxy = left_eye_xy[1]-eye_bbox_h//2
+
+###
+#  ——————————————
+# |             |
+# |      .      |
+# |             |
+# |             |
+# _______________
+###
+
+left_eye_im = input_img[
+    int(left_eye_xy[0]-eye_bbox_h//2):int(left_eye_xy[0]+eye_bbox_h//2),
+    int(left_eye_xy[1]-eye_bbox_w//2):int(left_eye_xy[1]+eye_bbox_w//2), :]
+
+#left_eye_im = left_eye_im[:,::-1,:] # No need for flipping left eye for iris detection
+right_eye_im = input_img[
+    int(right_eye_xy[0]-eye_bbox_h//2):int(right_eye_xy[0]+eye_bbox_h//2),
+    int(right_eye_xy[1]-eye_bbox_w//2):int(right_eye_xy[1]+eye_bbox_w//2), :]
+
 inp_left = cv2.cvtColor(left_eye_im, cv2.COLOR_RGB2GRAY)
 inp_left = cv2.equalizeHist(inp_left)
-inp_left = cv2.resize(inp_left, (180,108))[np.newaxis, ..., np.newaxis]
 
 inp_right = cv2.cvtColor(right_eye_im, cv2.COLOR_RGB2GRAY)
 inp_right = cv2.equalizeHist(inp_right)
-inp_right = cv2.resize(inp_right, (180,108))[np.newaxis, ..., np.newaxis]
 
 input_array = np.concatenate([inp_left, inp_right], axis=0)
+
+print(input_array/255 * 2 - 1)
 pred_left, pred_right = model.net.predict(input_array/255 * 2 - 1)
 
 lms_left = model._calculate_landmarks(pred_left)
 lms_right = model._calculate_landmarks(pred_right)
+
+input_array = np.concatenate([inp_left, inp_right], axis=0)
+pred_left, pred_right = model.net.predict(input_array/255 * 2 - 1)
 
 # g and b iris and pupil
 hm_g = np.max(pred_left[...,8:16], axis=-1, keepdims=True)
@@ -58,6 +78,21 @@ hm_b = np.max(pred_left[...,16:], axis=-1, keepdims=True)
 
 hm_g = np.max(pred_right[...,8:16], axis=-1, keepdims=True)
 hm_b = np.max(pred_right[...,16:], axis=-1, keepdims=True)
+
+lms_left = model._calculate_landmarks(pred_left)
+
+for i in lms_left:
+    print(i)
+
+lms_right = model._calculate_landmarks(pred_right)
+
+
+for i, lm in enumerate([left_eye_xy, right_eye_xy]):
+    draw = cv2.circle(draw, (int(lm[1]), int(lm[0])), 1, (255*i,255*(1-i),0), -1)
+    print(int(lm[1]), int(lm[0])) # x,y
+
+for lm in lms_left[0]:
+    draw = cv2.circle(draw, (int(left_bboxx + lm[0] * 3 ),int(left_bboxy + lm[1] * 3)), 1, (255,255,255), -1)
 
 cv2.imshow("image",draw)
 
